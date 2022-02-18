fc59d25456e73435c837f4fd07bdecea98f3d5a0
youyoung00 <you889@hanmail.net>
2022-02-18 11:59:16 +0900

    mysql-php connected

---
+++ html/db.php
@@ -0,0 +1,32 @@
+<?php 
+
+$mysql_hostname = 'host.docker.internal'; // 도커들끼리 통신하는 명령어
+
+$mysql_username = 'you889'; 
+
+$mysql_password = 'semin';
+
+$mysql_database = 'you889db';
+
+$mysql_port = '52000';
+
+$mysql_charset = 'UTF8';
+
+ 
+
+$connect = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port, $mysql_charset);
+
+ 
+
+if($connect->connect_errno){
+
+    echo '[연결실패..] : '.$connect->connect_error.'';
+
+}else{
+
+    echo '[연결성공!]'.'<br>';
+
+}
+ 
+
+?>

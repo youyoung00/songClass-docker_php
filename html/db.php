79bef867ab8cd3d806bb293e1210541aaf67e4e6
youyoung00 <you889@hanmail.net>
2022-02-18 12:21:07 +0900

    redme.md 파일 추가

--- html/db.php
+++ html/db.php
@@ -12,7 +12,7 @@ $mysql_port = '52000';
 
 $mysql_charset = 'UTF8';
 
- 
+
 
 $connect = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port, $mysql_charset);
 
---
+++ redme.md
@@ -0,0 +1,6 @@
+php apache mysql 기본설정 도커파일 세팅
+docker-compose build
+docker-compose up -d
+하면 
+웹서버 9001번
+데이터베이스 52000번에 세팅됨
